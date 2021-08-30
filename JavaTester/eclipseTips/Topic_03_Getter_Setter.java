1f13c95b2470b7b7a4cd782a8138f59ad93193b3
Thong Truong <thong3108@gmail.com>
2021-08-30 22:01:53 +0700

    Video 41 (Java Basic - Part VI: Java Keyword/ Convention)

---
+++ JavaTester/eclipseTips/Topic_03_Getter_Setter.java
@@ -0,0 +1,62 @@
+package eclipseTips;
+
+public class Topic_03_Getter_Setter {
+	// biến private bên ngoài ko thể truy cập dc, thông qua hàm, đó là hàm getter/setter
+	// tính đóng gói ko cho phép truy cập qua tên biến mà qua method
+	private String carName;
+	private String carType;
+	private String carColor;
+	private String carMachine;
+
+	/**
+	 * @return the carType
+	 */
+	String getCarType() {
+		return carType;
+	}
+
+	/**
+	 * @param carType the carType to set
+	 */
+	void setCarType(String carType) {
+		this.carType = carType;
+
+	}
+
+	/**
+	 * @return the carColor
+	 */
+	String getCarColor() {
+		return carColor;
+	}
+
+	/**
+	 * @param carColor the carColor to set
+	 */
+	void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+
+	/**
+	 * @return the carMachine
+	 */
+	String getCarMachine() {
+		return carMachine;
+	}
+
+	/**
+	 * @param carMachine the carMachine to set
+	 */
+	void setCarMachine(String carMachine) {
+		this.carMachine = carMachine;
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+}
---
+++ JavaTester/eclipseTips/Topic_04_Access_Modifier.java
@@ -0,0 +1,14 @@
+package eclipseTips;
+
+public class Topic_04_Access_Modifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Topic_03_Getter_Setter topic_03 = new Topic_03_Getter_Setter();
+		topic_03.setCarName("Huyndai Accent");
+
+		System.out.println(topic_03.getCarName());
+
+	}
+
+}
