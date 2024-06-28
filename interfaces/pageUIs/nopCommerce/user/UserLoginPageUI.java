d2aaea6c760f00c9664c3e9ec56f9a6d3da01ceb
thongdzui <42407481+thongdzui@users.noreply.github.com>
2024-06-28 16:12:24 +0700

    Update UserLoginPageUI.java
    edit locator

--- interfaces/pageUIs/nopCommerce/user/UserLoginPageUI.java
+++ interfaces/pageUIs/nopCommerce/user/UserLoginPageUI.java
@@ -2,10 +2,10 @@ package pageUIs.nopCommerce.user;
 
 public class UserLoginPageUI {
 
-	public static final String LOGIN_BUTTON = "xpath=//button[contains(@class,'login-button')]";
-	public static final String EMAIL_TEXTBOX = "xpath=//input[@id='Email']";
-	public static final String PASSWORD_TEXTBOX = "xpath=//input[@id='Password']";
-	public static final String EMAIL_ERROR_MESSAGE = "css=span[id='Email-error']";
-	public static final String UNSUCCESSFUL_ERROR_MESSAGE = "xpath=//div[contains(@class,'message-error')]";
+	public static final String LOGIN_BUTTON = "//button[contains(@class,'login-button')]";
+	public static final String EMAIL_TEXTBOX = "//input[@id='Email']";
+	public static final String PASSWORD_TEXTBOX = "//input[@id='Password']";
+	public static final String EMAIL_ERROR_MESSAGE = "span[id='Email-error']";
+	public static final String UNSUCCESSFUL_ERROR_MESSAGE = "//div[contains(@class,'message-error')]";
 
 }
